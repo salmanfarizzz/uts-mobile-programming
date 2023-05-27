48d104e9fdb88db0696dc8f991521c6813d89727
salmanfarizzz <98375213+salmanfarizzz@users.noreply.github.com>
2023-05-27 09:57:38 +0700

    Create Onboarding2.dart

---
+++ Onboarding2.dart
@@ -0,0 +1,154 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'onboarding2_model.dart';
+export 'onboarding2_model.dart';
+
+class Onboarding2Widget extends StatefulWidget {
+  const Onboarding2Widget({Key? key}) : super(key: key);
+
+  @override
+  _Onboarding2WidgetState createState() => _Onboarding2WidgetState();
+}
+
+class _Onboarding2WidgetState extends State<Onboarding2Widget> {
+  late Onboarding2Model _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final _unfocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => Onboarding2Model());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    _unfocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.6,
+              decoration: BoxDecoration(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: Image.asset(
+                    'assets/images/42e59cc6-2153-47ff-8395-4c2bad9d49a3.jpg',
+                  ).image,
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(300, 40, 20, 0),
+                child: Text(
+                  'Skip',
+                  style: FlutterFlowTheme.of(context).titleMedium.override(
+                        fontFamily: 'Poppins',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: AlignmentDirectional(0.3, 0),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                child: Text(
+                  'BERBAGAI KEUNTUNGAN VIA FORE COFFEE APP',
+                  style: FlutterFlowTheme.of(context).titleLarge.override(
+                        fontFamily: 'Poppins',
+                        color: Color(0xFF00AA13),
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: AlignmentDirectional(-0.35, 0),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(19, 20, 20, 0),
+                child: Text(
+                  'Pesan tanpa antri, dapat promo menarik setiap hari, Fore Poin, dan berbagai promo lainnya',
+                  style: FlutterFlowTheme.of(context).titleSmall.override(
+                        fontFamily: 'Poppins',
+                        color: FlutterFlowTheme.of(context).secondaryText,
+                      ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 80, 0, 0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).accent3,
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                          color: FlutterFlowTheme.of(context).accent2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(10, 50, 10, 0),
+                    child: Container(
+                      width: 25,
+                      height: 25,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00AA13),
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                          color: Color(0xFF00AA13),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).accent3,
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                          color: FlutterFlowTheme.of(context).accent2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
